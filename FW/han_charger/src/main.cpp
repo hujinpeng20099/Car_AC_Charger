99caca49b2e667133bbdb19aade140cb7e4cb451
hujinpeng20099 <hujinpeng20088@163.com>
2022-08-15 15:23:49 +0800

    add lvgl and touch lib     Changes to be committed: 	new file:   .gitmodules 	new file:   FW/han_charger/lib/Arduino-FT6336U 	modified:   FW/han_charger/platformio.ini 	modified:   FW/han_charger/src/main.cpp

--- FW/han_charger/src/main.cpp
+++ FW/han_charger/src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include "lvgl.h"
 
 TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(1,OUTPUT);
+  lv_init();
 }
 
 void loop() {
